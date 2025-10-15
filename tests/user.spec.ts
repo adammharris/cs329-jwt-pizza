50bef415f345d81f26885728efed46d485830b6f
adammharris <97053407+adammharris@users.noreply.github.com>
2025-10-14 23:24:30 -0600

    stubbed out admin dashboard user table

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -32,3 +32,37 @@ test("updateUser", async ({ page }) => {
 
   await expect(page.getByRole("main")).toContainText("pizza dinerx");
 });
+
+test("listUsers", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill("a@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("admin");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByRole("link", { name: "Admin" }).click();
+  // As an admin I can see a list of all users. Each user's name, email, and role is displayed. The list is paginated. The list can be filtered by user name.
+  await expect(page.getByRole("main")).toContainText("Mama Ricci's kitchen");
+  
+  const userRow = page.getByRole("row", {
+    name: /pizza dinerx pizza dinerx user\d+@jwt\.com diner/,
+  });
+  await expect(userRow).toBeVisible();
+});
+
+test("deleteUser", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill("a@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).fill("admin");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByRole("link", { name: "Admin" }).click();
+
+  //As an admin I can delete any user.
+  const userRow = page.getByRole("row", {
+    name: /pizza dinerx pizza dinerx user\d+@jwt\.com diner/,
+  });
+  await expect(userRow).toBeVisible();
+  await userRow.getByRole("button", { name: "Delete" }).click();
+  await page.getByRole("button", { name: "Yes, I'm sure" }).click();
+  await expect(userRow).toHaveCount(0);
+});
